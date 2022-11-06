8bfabf16c6ec6f3ebf248cfaa3d5041967b473a8
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-11-05 21:09:26 -0600

    feat:  State changes working, need to clean up UI and stuff

--- lib/main.dart
+++ lib/main.dart
@@ -4,35 +4,130 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  String _bannerText = 'Butter sux?';
+
   @override
   Widget build(BuildContext context) {
-    const double defaultFontSize = 36;
-    const String greetingText = "Hey Miss Alijah!";
-
-    const textWidget = Text(
-      'Cook Faster I am hungreee!!! (ps butter sux)',
-      style: TextStyle(fontSize: defaultFontSize),
-    );
-
-    const paddingWidget = Padding(
-        padding: EdgeInsets.all(15.0), child: Center(child: textWidget));
-
-    var columnWidget = Column(
-      children: const [Center(child: paddingWidget)],
-    );
-
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.lime,
         ),
         home: Scaffold(
-            appBar: AppBar(title: const Center(child: Text(greetingText))),
+            appBar: AppBar(title: const Center(child: Text('Hello Alijah'))),
             body: Padding(
-                padding: const EdgeInsets.all(15.0), child: columnWidget)));
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: [
+                    Text(_bannerText),
+                    Row(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _bannerText = 'Correct!';
+                            });
+                          },
+                          child: const Text('Yes?'),
+                        ),
+                        ElevatedButton(
+                            onPressed: () => setState(() {
+                                  _bannerText = 'WRONG!!!!!';
+                                }),
+                            child: const Text('No'))
+                      ],
+                    )
+                  ],
+                ))));
   }
 }
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class _BannerTextState extends State<MyApp> {
+//   String _statusMessage = 'Butter sux?';
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text(_statusMessage);
+//   }
+// }
+//
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+//
+//   String statusMessage;
+//
+//   void updateWithYes() {
+//
+//   }
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     const double defaultFontSize = 36;
+//     const String greetingText = "Hey Miss Alijah!";
+//
+//     const textWidget = Text(
+//       'Cook Faster I am hungreee!!! (ps butter suxzzz)',
+//       style: TextStyle(fontSize: defaultFontSize),
+//     );
+//
+//     const paddingWidget = Padding(
+//         padding: EdgeInsets.all(15.0), child: Center(child: textWidget));
+//
+//     var columnWidget = Column(
+//       children: const [Center(child: paddingWidget)],
+//     );
+//
+//     String statusMessage = "Butter Suxzzzz?";
+//
+//     const statusLabel = Text('Butter sux?');
+//
+//     return MaterialApp(
+//         title: 'Flutter Demo',
+//         theme: ThemeData(
+//           primarySwatch: Colors.lime,
+//         ),
+//         home: Scaffold(
+//             appBar: AppBar(title: const Center(child: Text('Hello Alijah'))),
+//             body: Padding(
+//                 padding: const EdgeInsets.all(15.0),
+//                 child: Column(
+//                   children: [
+//                     Text(statusMessage),
+//                     Row(
+//                       children: [
+//                         ElevatedButton(
+//                           child: const Text('Yes'),
+//                           onPressed: () => statusLabel. = 'Correct',
+//                         ),
+//                         ElevatedButton(
+//                           child: const Text('No'),
+//                           onPressed: () => {},
+//                         )
+//                       ],
+//                     )
+//                   ],
+//                 ))));
+//   }
+//
+//   @override
+//   State<StatefulWidget> createState() {
+//     statusMessage = 'Butter sux?';
+//   }
+// }
